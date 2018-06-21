9963b2b196b28e5deba13bcdde235334f81c3f11
Woojun Choi <tim.woojune1@gmail.com>
2018-06-21 01:17:04 -0700

    auth error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <a href='http://localhost:8888'>
+        <button>Login with Spotify</button>
+      </a>
       </div>
     );
   }
